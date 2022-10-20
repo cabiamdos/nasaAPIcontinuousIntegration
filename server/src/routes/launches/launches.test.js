a62687da939120201149a28ec052585b60cac73f
William <cabiam@criptext.com>
2022-10-20 12:08:16 +0200

    updated node.yml and launchest.test.js

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,10 +1,12 @@
 const request = require('supertest');
 const app = require('../../app');
+const { loadPlanetData } = require('../../models/planets.model');
 const {mongoConnect, mongoDisconnect} = require('./../../services/mongo');
 
 describe('Launches API', () => {
   beforeAll(async() => {
     await mongoConnect();
+    await loadPlanetData();
   });
 
   afterAll(async () => {
